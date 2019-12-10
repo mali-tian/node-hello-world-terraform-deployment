20306b9bb11a6f786c5c29c306ee65569b802420
mali.tian <mali.tian@rea-group.com>
2019-12-10 21:54:02 +0800

    create execution role for ecs task definition

--- deployment.tf
+++ deployment.tf
@@ -17,13 +17,75 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_task_execution_cloudwatch_logs_policy" {
+  name = "ecs_task_execution_cloudwatch_logs_policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+  name = "ecs_task_execution_policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecs_cluster" "node-hello-world-terraform" {
   name = "node-hello-world-terraform"
 }
 
 resource "aws_ecs_task_definition" "node-hello-world-terraform" {
   cpu                      = "256"
-  execution_role_arn       = "<ROLE_ARN>"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   family                   = "hello-world-task-terraform"
   memory                   = "512"
   network_mode             = "awsvpc"
